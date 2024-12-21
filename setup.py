feb59d30f2684bb90d15acec46ae0ea09d98f9e7
rajasekar-m <paviraj2015@gmail.com>
2024-12-21 22:49:48 +0530

    req

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages,  setup
+
+setup(
+
+    name = 'MLpipeline',
+    version= '0.0.0',
+    author= 'RJ',
+    author_email= 'paviraj2015@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+
+
+)
